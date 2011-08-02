33cce8ec9a793395311bfad78360603d0953b03b
Sergey <bioramaenator@gmail.com>
2011-08-03 03:05:27 +0400

    fix cport.c

--- src/cport.c
+++ src/cport.c
@@ -8,16 +8,10 @@ typedef unsigned char byte;
 int read_cmd(byte *buf)
 {
   int len;
-  //fprintf(stderr, "try read\r\n");
   if (read_exact(buf, 4) != 4)
-    {
-      //fprintf(stderr, "bad: %s\r\n", strerror(errno));
       return(-1);
-    }
 
-  //fprintf(stderr, "ll: %d, %d, %d, %d\r\n", buf[0], buf[1], buf[2], buf[3]);
   len = (buf[0] << 24) | (buf[1] << 16) | (buf[2] << 8) | buf[3];
-  //fprintf(stderr, "#len: %d\r\n", len);
   return read_exact(buf, len);
 }
 
@@ -29,9 +23,7 @@ int write_cmd(byte *buf, int len)
   li[2] = (len >> 8)  & 0xff;
   li[3] = len  & 0xff;
   write_exact(&li, 4);
-  
-  //fprintf(stderr, "last b: %d\r\n", li[3]);
-  //fprintf(stderr, "actualy reply len: %d\r\n", len);
+
   return write_exact(buf, len);
 }
 
@@ -39,15 +31,12 @@ int read_exact(byte *buf, int len)
 {
   int i, got=0;
   do {
-    //fprintf(stderr, "read %d bytes\r\n", len - got);
     if ((i = read(0, buf+got, len-got)) <= 0)
       {
-	//fprintf(stderr, "bad1: %s, return %d\r\n", strerror(errno), i);
 	return(i);
       }
     got += i;
   } while (got<len);
-  //fprintf(stderr, "len: %d\r\n", len);
   return len;
 }
 
